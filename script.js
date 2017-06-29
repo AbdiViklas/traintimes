6f1303b1644711d3bac295229892132d4ea88e05
Andy Bonner <abonner@duke.edu>
2017-06-29 12:24:17 -0400

    begin building trainArray, link moment.js

--- script.js
+++ script.js
@@ -8,3 +8,41 @@ var config = {
   messagingSenderId: "219479184128"
 };
 firebase.initializeApp(config);
+
+
+var trainArray = [
+  {
+    name: "Hogwarts Express",
+    destination: "Hogsmeade",
+    frequency: 87658,
+    daily: false,
+    firstDay: "09 01 2017",
+    firstTime: "1100"
+  },
+  {
+    name: "Polar Express",
+    destination: "N. Pole",
+    frequency: 525600,
+    daily: false,
+    firstDay: "12 24 2017",
+    firstTime: "2355"
+  },
+  {
+    name: "Orient Express",
+    destination: "Istanbul",
+    frequency: 525600,
+    daily: true,
+    firstDay: "",
+    firstTime: "2355"
+  },
+];
+
+
+/*
+pseudocode for calculating times
+
+take user input and convert to 24-hr
+get current time
+calculate all the departure times for the day (store in local array--or in Firebase array?)
+check for first departure time which is after current time
+*/
