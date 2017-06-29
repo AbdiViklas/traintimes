1df9853ebfd9b95a9852d13acc02cc78fcf8d982
Andy Bonner <abonner@duke.edu>
2017-06-29 16:52:43 -0400

    create add train form

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var backgroundCount = 0;
+var backgrounds = ["#efebe9", 
+"#efebe9 url('images/golden_rail.jpg') no-repeat center/cover", 
+"#efebe9 url('images/misty_rail.jpg') no-repeat center/cover"];
 
 var trainArray = [
   {
@@ -46,3 +50,59 @@ get current time
 calculate all the departure times for the day (store in local array--or in Firebase array?)
 check for first departure time which is after current time
 */
+
+$("#background").click(function (e) { 
+  e.preventDefault();
+  // iterate repeatedly
+  if (backgroundCount < backgrounds.length - 1) {
+   backgroundCount++; 
+  } else {
+    backgroundCount = 0;
+  }
+  console.log("backgroundCount is", backgroundCount);
+  // card background to white on solid body background; otherwise rgba
+  if (backgroundCount === 0) {
+    $(".card").css("background-color", "");
+  } else {
+    $(".card").css("background-color", "rgba(255, 255, 255, 0.8)");
+  }
+  // set body background from array
+  $("body").css("background", backgrounds[backgroundCount]);
+});
+
+// initialize Material's dropdown selector
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+$("#daily").on("change", function () {
+  $("#datePick").toggle(200);
+  if ($(this).is(':checked')) {
+    $("#optionDay").attr("disabled", "");
+    $(".lessThanDay").removeAttr("disabled");
+    $("#timeVal").material_select();
+  } else {
+    $("#optionDay").removeAttr("disabled");
+    $(".lessThanDay").attr("disabled", "");
+    $("#timeVal").material_select();
+  }
+});
+
+// initialize datepicker
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 2 // Creates a dropdown of 2 years to control year
+});
+
+// initialize timepicker
+$('.timepicker').pickatime({
+    default: 'now', // Set default time
+    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+    twelvehour: false, // Use AM/PM or 24-hour format
+    donetext: 'OK', // text for done-button
+    cleartext: 'Clear', // text for clear-button
+    canceltext: 'Cancel', // Text for cancel-button
+    autoclose: false, // automatic close timepicker
+    ampmclickable: true, // make AM PM clickable
+    aftershow: function(){} //Function for after opening timepicker  
+  });
