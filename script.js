87730dc323437fb9bc421c013bb19a47b1dcd7e8
Andy Bonner <abonner@duke.edu>
2017-06-29 11:12:58 -0400

    link and initialize Firebase

---
+++ script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBypclwUAe-PreKQtB2qekbgsF6km5Xfvg",
+  authDomain: "traintimes-71265.firebaseapp.com",
+  databaseURL: "https://traintimes-71265.firebaseio.com",
+  projectId: "traintimes-71265",
+  storageBucket: "",
+  messagingSenderId: "219479184128"
+};
+firebase.initializeApp(config);
