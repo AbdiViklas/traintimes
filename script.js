bff2e9c2d055e1ad5b7c4ba40055a55a85bc652f
Andy Bonner <abonner@duke.edu>
2017-06-30 10:43:19 -0400

    finish buildRow()

--- script.js
+++ script.js
@@ -56,29 +56,53 @@ check for first departure time which is after current time
 function buildRow(obj) {
   console.log("object received as", obj)
   var newRow = $("<tr>");
-  function addTd(key) {
+  function addTd(string) {
     var newTd = $("<td>");
-    newTd.text(obj[key]);
+    newTd.text(string);
     newRow.append(newTd);
   }
-  addTd("name");
-  addTd("destination");
-  addTd("frequency");
-  var start;
-  if (obj.firstDay !== "") {
-    start = moment(obj.firstDay, "D MMM, YYYY");
+  addTd(obj.name);
+  addTd(obj.destination);
+  addTd(obj.frequency);
+  var start, startDate, startTime; // build the start date and time
+  var daily = false;
+  if (obj.firstDay === "") {
+    // if firstDay is blank, default to today
+    startDate = moment().format("D MMM, YYYY");
+    daily = true; // since we're doing this check already, save the outcome to use below when formatting for date vs time
   } else {
-    start = moment(obj.firstTime, "HH:mm");
+    startDate = obj.firstDay;
   }
-  // if first departure is in future, name it in Next Arrival and simply subtract for Min Away
-  // if in past, add multiples of frequency until we reach a time later than present;
-  // then name and subtract
-  addTd("firstTime"); //IMPORTANT: remove; for testing only
-  // delete below
-  var testTd = $("<td>");
-  testTd.text("4");
-  newRow.append(testTd);
+  startTime = obj.firstTime;
+  start = moment(startDate + " " + startTime, "D MMM, YYYY HH:mm");
+  console.log("start:", start);
+
+  var now = moment();
+  console.log("now:", now);
+  var diffFirst = now.diff(start, "minutes");
+  console.log("difFirst:", diffFirst);
+  var incrementedTime;
+  if (diffFirst > 0) { // if start is in the past...
+    console.log("in the past");
+    do {
+      incrementedTime = start.add(obj.frequency, "m"); // multiply by frequency...
+    } while (now.diff(incrementedTime, "minutes") > 0); // until we reach a time later than present.
+    console.log("incrementedTime:", incrementedTime);
+    console.log("After incrementing from past start time by frequency, the next departure is", incrementedTime.format("M/D/YY, HH:mm"));
+    start = incrementedTime; // update start to the next future departure
+    diffFirst = now.diff(start, "minutes"); // update diffFirst to new (negative) value
+  }
+  // outside that if block, we can assume start is in future
+  // format "Next Arrival" to display date for future dates, or just time for today
+  if (daily) { // set above
+    addTd(start.format("HH:mm")); // name it in Next Arrival, formated as 24-hr time ...
+  } else {
+    addTd(start.format("M/D/YY")); // or else as a day.
+  }
+  addTd(diffFirst * -1); // Finally, use the difference (flipped back to a positive integer) for Min Away
+
   $("#table-body").append(newRow);
+  console.log("/////////////////")
 }
 
 $("#background").click(function (e) { 
@@ -99,11 +123,12 @@ $("#background").click(function (e) {
   $("body").css("background", backgrounds[backgroundCount]);
 });
 
-// initialize Material's dropdown selector
+// initialize Materialize's dropdown selector
 $(document).ready(function() {
     $('select').material_select();
 });
 
+// listen for selection of the "daily" switch in the form to enable and disable options and reinitialize Materialize selector
 $("#daily").on("change", function () {
   $("#datePick").toggle(200);
   if ($(this).is(':checked')) {
@@ -117,13 +142,13 @@ $("#daily").on("change", function () {
   }
 });
 
-// initialize datepicker
+// initialize Materialize datepicker
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 2 // Creates a dropdown of 2 years to control year
 });
 
-// initialize timepicker
+// initialize Materialize timepicker
 $('.timepicker').pickatime({
     default: 'now', // Set default time
     fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
@@ -133,15 +158,16 @@ $('.timepicker').pickatime({
     canceltext: 'Cancel', // Text for cancel-button
     autoclose: false, // automatic close timepicker
     ampmclickable: true, // make AM PM clickable
-    aftershow: function(){} //Function for after opening timepicker  
   });
 
 
 $("#add-form").submit(function (e) {
   e.preventDefault();
   var dataObj = {};
+  // build object properties one by one from form values
   dataObj.name = $("#name").val();
   dataObj.destination = $("#destination").val();
+  // calculate minutes for "frequency"
   var frequencyVal = $("#frequency").val();
   switch ($("#timeVal").val()) {
     case "min":
@@ -158,10 +184,11 @@ $("#add-form").submit(function (e) {
   }
   dataObj.firstDay = $("#firstDay").val();
   dataObj.firstTime = $("#firstTime").val();
-  database.ref().push(dataObj);
+  database.ref().push(dataObj); // push to Firebase
+  document.getElementById("add-form").reset(); // reset the form--
+  // take care of re-hiding datepicker, enabling/disabling selector, and re-intializing
 });
 
 database.ref().on("child_added", function(snapshot){
-  console.log(snapshot.key, snapshot.val());
   buildRow(snapshot.val());
 });
